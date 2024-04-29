eb09ce2eb445a4f4b2e7958a7b4872f717f8118e
shiro <24359398+actuarysailor@users.noreply.github.com>
2024-04-28 23:35:33 -0400

    Update __init__.py
    Minor updates to replace the deprecated conint method from pydantic with Annotated from typing_extensions

--- repo_manager/schemas/__init__.py
+++ repo_manager/schemas/__init__.py
@@ -1,65 +1,84 @@
-import yaml
 from pydantic import BaseModel  # pylint: disable=E0611
+from typing_extensions import Annotated
+from pydantic import Field
 
-from .branch_protection import BranchProtection
-from .file import FileConfig
-from .label import Label
-from .secret import Secret
-from .settings import Settings
-from .environment import environment
-from .collaborator import Collaborator
+OptBool = bool | None
+OptStr = str | None
 
 
-class RepoManagerConfig(BaseModel):
-    settings: Settings | None
-    branch_protections: list[BranchProtection] | None
-    secrets: list[Secret] | None
-    variables: list[Secret] | None
-    labels: list[Label] | None
-    files: list[FileConfig] | None
-    environments: list[environment] | None
-    collaborators: list[Collaborator] | None
-
-    @property
-    def secrets_dict(self):
-        return {secret.key: secret for secret in self.secrets} if self.secrets is not None else {}
-
-    @property
-    def variables_dict(self):
-        return {variable.key: variable for variable in self.variables} if self.variables is not None else {}
-
-    @property
-    def labels_dict(self):
-        return {label.expected_name: label for label in self.labels} if self.labels is not None else {}
-
-    @property
-    def branch_protections_dict(self):
-        return (
-            {branch_protection.name: branch_protection for branch_protection in self.branch_protections}
-            if self.branch_protections is not None
-            else {}
-        )
-
-    @property
-    def environments_dict(self):
-        return (
-            {environment.name: environment for environment in self.environments}
-            if self.environments is not None
-            else {}
-        )
-
-    @property
-    def collaborators_dict(self):
-        return (
-            {collaborator.name: collaborator for collaborator in self.collaborators}
-            if self.collaborators is not None
-            else {}
-        )
+class RestrictionOptions(BaseModel):
+    users: list[str] | None = Field(
+        None, description="List of users who cannot push to this branch, only available to orgs"
+    )
+    teams: list[str] | None = Field(
+        None, description="List of teams who cannot push to this branch, only available to orgs"
+    )
 
 
-def load_config(filename: str) -> RepoManagerConfig:
-    """Loads a yaml file into a RepoManagerconfig"""
-    with open(filename) as fh:
-        this_dict = yaml.safe_load(fh)
+class StatusChecksOptions(BaseModel):
+    strict: OptBool = Field(False, description="Require branches to be up to date before merging.")
+    checks: list[str] | None = Field(
+        [], description="The list of status checks to require in order to merge into this branch"
+    )
 
-    return RepoManagerConfig(**this_dict)
+
+class DismissalOptions(BaseModel):
+    users: list[str] | None = Field(
+        None, description="List of users who can dismiss pull request reviews, only available to orgs"
+    )
+    teams: list[str] | None = Field(
+        None, description="List of teams who can dismiss pull request reviews, only available to orgs"
+    )
+
+
+class PROptions(BaseModel):
+    required_approving_review_count: Annotated[int, Field(strict=True, gt=1, le = 6)] | None = Field(
+        None, description="The number of approvals required. (1-6)"
+    )
+    dismiss_stale_reviews: OptBool = Field(
+        None, description="Dismiss approved reviews automatically when a new commit is pushed."
+    )
+    require_code_owner_reviews: OptBool = Field(None, description="Blocks merge until code owners have reviewed.")
+    dismissal_restrictions: DismissalOptions | None = Field(
+        None, description="Options related to PR dismissal. Only available to Orgs. Not available in the Check command"
+    )
+
+
+class ProtectionOptions(BaseModel):
+    pr_options: PROptions | None = Field(None, description="Options related to PR reviews")
+    required_status_checks: StatusChecksOptions | None = Field(
+        StatusChecksOptions(), description="Options related to required status checks"
+    )
+    enforce_admins: OptBool = Field(
+        None,
+        description="Enforce all configured restrictions for administrators. Set to true to enforce required status "
+        + "checks for repository administrators. Set to null to disable.",
+    )
+    require_linear_history: OptBool = Field(
+        None, description="Prevent merge commits from being pushed to matching branches"
+    )
+    restrictions: RestrictionOptions | None = Field(
+        None, description="Options related to restricting who can push to this branch"
+    )
+    allow_force_pushes: OptBool = Field(None, description="Permit force pushes for all users with push access.")
+    allow_deletions: OptBool = Field(None, description="Allow users with push access to delete matching branches.")
+    block_creations: OptBool = Field(
+        None,
+        description="If set to true, the restrictions branch protection settings which limits who can push "
+        + "will also block pushes which create new branches, unless the push is initiated by a user, team, or "
+        + "app which has the ability to push. Set to true to restrict new branch creation.",
+    )
+
+    require_conversation_resolution: OptBool = Field(
+        None,
+        description="When enabled, all conversations on code must be resolved before a pull request can be merged.",
+    )
+    require_signed_commits: OptBool = Field(
+        None, description="Commits pushed to matching branches must have verified signatures."
+    )
+
+
+class BranchProtection(BaseModel):
+    name: OptStr = Field(None, description="Name of the branch")
+    protection: ProtectionOptions | None = Field(None, description="Protection options for the branch")
+    exists: OptBool = Field(True, description="Set to false to delete a branch protection rule")
