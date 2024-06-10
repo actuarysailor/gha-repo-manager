4d85653038aece692d8104885ac851bcfbb76fa3
shiro <24359398+actuarysailor@users.noreply.github.com>
2024-06-10 09:56:09 -0400

    Update test_main.py

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -3,6 +3,7 @@ import os
 from repo_manager.main import main
 
 
+os.environ["GITHUB_SERVER_URL"] = "https://github.com"
 os.environ["INPUT_REPO"] = "self"
 os.environ["INPUT_ACTION"] = "check"
 os.environ["INPUT_SETTINGS_FILE"] = ".github/settings.yml"
