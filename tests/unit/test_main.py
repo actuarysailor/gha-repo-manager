a62b9b2b725b1421f35f4e3c05ba0c35aa42e937
actuarysailor <24359398+actuarysailor@users.noreply.github.com>
2024-06-10 05:15:14 -0400

    ci: Fix test environment variables

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -3,7 +3,6 @@ import os
 from repo_manager.main import main
 
 
-os
 os.environ["INPUT_REPO"] = "self"
 os.environ["INPUT_ACTION"] = "check"
 os.environ["INPUT_SETTINGS_FILE"] = ".github/settings.yml"
